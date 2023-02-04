957fd8c9dfb5588962cfe888e1e59b292de505c8
ithiel883 <ajuka.zephiniah@gmail.com>
2023-02-04 20:30:50 +0100

    started layout for sign up page

---
+++ pages/signup.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full flex">
+        <div class="w-1/2 justify-center flex bg-yellow-300 items-center h-screen">
+            <div class="text-4xl">
+                Hello 👋🏼
+            </div>
+        </div>
+        <div class="w-1/2 justify-center flex bg-red-300 items-center h-screen">
+            <div class="text-4xl">
+                Hola 👋🏼
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
