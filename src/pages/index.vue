7ae041380f885bae0ab862b25d1fdd8bacdee9b5
ithiel883 <ajuka.zephiniah@gmail.com>
2023-07-12 17:28:59 +0100

    started slicing of view posts page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div>
-    this is le index
+    <div class="mx-4">
+      <span class="text-2xl text-signupBorder font-bold my-4">Recent posts</span>
+      <div class="d-flex flex-col gap-4 w-full h-screen">
+        <div v-for="post in posts" :key="post.id" class="w-full  border border-signupBorder">
+          <div class="w-full">
+            <div>
+              <img v-if="post.image" :src="post.image" class="w-full" />
+              <img v-else src="../assets/download.png" class="w-full" />
+            </div>
+            <span>{{ post.title }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -9,7 +22,7 @@
 import cookie from 'cookiejs'
 import { getAllPosts } from '../services/post-service'
 let $router = useRouter()
-
+const posts = ref<any>([])
 
 onMounted(() => {
 
@@ -17,7 +30,7 @@ onMounted(() => {
 
 onMounted(async () => {
   let val = await getAllPosts()
-  console.log(val)
+  posts.value = val.data
   let token = cookie.get("token")
   if (!token) {
     $router.push('/signup')
